5783398cfb731055d7fd386d1364ee27443fb9d9
Yanminge <myan@makeblock.com>
2018-09-19 18:13:15 +0800

    first commit for pepper project

---
+++ app/src/main/java/com/example/yanminge/pepper_project/main_activity.java
@@ -0,0 +1,48 @@
+package com.example.yanminge.pepper_project;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.aldebaran.qi.sdk.QiContext;
+import com.aldebaran.qi.sdk.QiSDK;
+import com.aldebaran.qi.sdk.RobotLifecycleCallbacks;
+import com.aldebaran.qi.sdk.builder.SayBuilder;
+import com.aldebaran.qi.sdk.object.conversation.Say;
+
+public class main_activity extends AppCompatActivity implements RobotLifecycleCallbacks {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity);
+        QiSDK.register(this, this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        // Unregister the c for this Activity.
+        QiSDK.unregister(this, this);
+        super.onDestroy();
+    }
+
+    @Override
+    public void onRobotFocusGained(QiContext qiContext) {
+        // Create a new say action.
+        Say say = SayBuilder.with(qiContext) // Create the builder with the context.
+                .withText("Hello hujinghong!") // Set the text to say.
+                .build(); // Build the say action.
+
+        // Execute the action.
+        say.run();
+    }
+
+    @Override
+    public void onRobotFocusLost() {
+        // Nothing here.
+    }
+
+    @Override
+    public void onRobotFocusRefused(String reason) {
+        // Nothing here.
+    }
+}
